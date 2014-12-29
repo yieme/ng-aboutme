00b1ad0b9bc675c423b6e1376e6cbce512b63436
jim <jim@jims-MacBook-Pro.local>
2014-12-28 21:40:21 -0700

    initial

---
+++ ng-aboutme.js
@@ -0,0 +1,33 @@
+angular.module('yieme.aboutme', [])
+.factory('$aboutMe', ['$http', '$log', '$q', function ($http, $log, $q) {
+  return {
+    get: function(url) {
+      var deferred = $q.defer(),
+          node     = Math.floor(Math.random() * 2) + 1,
+          url      = url || 'http://ajaxhttpheaders' + node + '.appspot.com';
+
+      $http.jsonp(url + '?callback=JSON_CALLBACK')
+      .success(function(data) {
+        var language = data['Accept-Language'] || '',
+        geo          = data['X-Appengine-Citylatlong'] || ',',
+        latitude     = geo.split(',')[0],
+        longitude    = geo.split(',')[1],
+        result       = {
+          "language":  language.split(';')[0].split(',')[0],
+          "city":      data['X-Appengine-City'],
+          "region":    data['X-Appengine-Region'],
+          "country":   data['X-Appengine-Country'],
+          "latitude":  (latitude.length > 0)  ? parseFloat(latitude)  : undefined,
+          "longitude": (longitude.length > 0) ? parseFloat(longitude) : undefined,
+          "ua":        data['User-Agent']
+        };
+        deferred.resolve(result);
+      }).error(function(msg, code) {
+        deferred.reject(msg);
+        $log.error(msg, code);
+      });
+
+      return deferred.promise;
+    }
+  }
+}]);
